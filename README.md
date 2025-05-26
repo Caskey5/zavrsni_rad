22ae4711b3864220d4f427fffd0d91a851fdd917
Caskey5 <siljacantonio@gmail.com>
2025-05-26 17:53:48 +0200

    zavrsni rad

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# zavrsni_rad
+"# machina_python" 
+
+Za testiranje:
+
+Amdin nadimak -> Caskey
+Admin lozinka -> #Caskey123
+
+Zaposlenik nadimak -> I1
+Zaposlenik lozinka -> #I1
+
+Za korisnika se mozete "signup-ati" i testirati sa svojim podacima.
--- validation/validators.py
+++ validation/validators.py
@@ -1,9 +1,17 @@
 import re
 
 def is_valid_password(password):
-    # Bar jedno veliko slovo, broj i poseban znak
-    if (re.search(r"[A-Z]", password) and
-        re.search(r"\d", password) and
-        re.search(r"[^\w\s]", password)):
-        return True
-    return False
+    """
+    Validates that password contains:
+    - At least one uppercase letter
+    - At least one number
+    - At least one special character
+    """
+    if len(password) < 8:
+        return False
+    
+    has_upper = bool(re.search(r'[A-Z]', password))
+    has_digit = bool(re.search(r'\d', password))
+    has_special = bool(re.search(r'[!@#$%^&*(),.?":{}|<>]', password))
+    
+    return has_upper and has_digit and has_special 
